45d61dd1c0fa6e66827264e6ca3dd31d8998a611
wjdtkdgns777 <wjdtkdgns234@naver.com>
2021-06-14 23:26:57 +0900

    test for branch

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter test',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
