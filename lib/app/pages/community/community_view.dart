b3b9603faf7e08ae3d0e45e4289b0939697dafd0
JSH <wjdtkdgns234@naver.com>
2021-06-25 17:51:05 +0900

    Refactoring

--- lib/app/pages/community/community_view.dart
+++ lib/app/pages/community/community_view.dart
@@ -1,79 +1,79 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-//
-// import 'package:flutter/material.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:internet_connection_checker/internet_connection_checker.dart';
-// import 'package:synergy_flutter/app/components/stream_builder_wrapper.dart';
-// import 'package:synergy_flutter/data/utils/firebase.dart';
-// import 'package:synergy_flutter/domain/entities/post.dart';
-//
-// class Community extends StatefulWidget {
-//   @override
-//   _CommunityState createState() => _CommunityState();
-// }
-//
-// class _CommunityState extends State<Community> {
-//
-//   final  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       key: scaffoldKey,
-//       appBar: AppBar(
-//         automaticallyImplyLeading: false,
-//         title: Text(
-//           'Community',
-//           style: TextStyle(fontWeight: FontWeight.w900),
-//         ),
-//         centerTitle: false,
-//         // actions: [
-//         //   IconButton(
-//         //     icon: Icon(CupertinoIcons.chat_bubble_2_fill,
-//         //         size: 30.0, color: Theme.of(context).accentColor),
-//         //     onPressed: () {
-//         //       Navigator.push(
-//         //           context, CupertinoPageRoute(builder: (_) => Chats()));
-//         //     },
-//         //   ),
-//         //   SizedBox(width: 20.0),
-//         // ],
-//       ),
-//       body: ListView(
-//         shrinkWrap: true,
-//         padding: EdgeInsets.symmetric(horizontal: 10.0),
-//         children: [
-//           StreamBuilderWrapper(
-//             shrinkWrap: true,
-//             stream: postRef.orderBy('timestamp', descending: true).snapshots(),
-//             physics: NeverScrollableScrollPhysics(),
-//             itemBuilder: (_, DocumentSnapshot snapshot) {
-//               internetChecker();
-//               PostModel posts = PostModel.fromJson(snapshot.data());
-//               return Padding(
-//                 padding: const EdgeInsets.only(bottom: 12.0),
-//                 //  child: Posts(post: posts),
-//                 child: UserPost(post: posts),
-//               );
-//             },
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-//
-//   internetChecker() async {
-//     bool result = await InternetConnectionChecker().hasConnection;
-//     if (result == false) {
-//       showInSnackBar('No Internet Connecton');
-//     }
-//   }
-//
-//   void showInSnackBar(String value) {
-//     scaffoldKey.currentState.removeCurrentSnackBar();
-//     scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(value)));
-//   }
-// }
-//
-//
+import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:synergy_flutter/app/components/stream_builder_wrapper.dart';
+import 'package:synergy_flutter/data/utils/firebase.dart';
+import 'package:synergy_flutter/domain/entities/post.dart';
+
+class Community extends StatefulWidget {
+  @override
+  _CommunityState createState() => _CommunityState();
+}
+
+class _CommunityState extends State<Community> {
+
+  final  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Community',
+          style: TextStyle(fontWeight: FontWeight.w900),
+        ),
+        centerTitle: false,
+        // actions: [
+        //   IconButton(
+        //     icon: Icon(CupertinoIcons.chat_bubble_2_fill,
+        //         size: 30.0, color: Theme.of(context).accentColor),
+        //     onPressed: () {
+        //       Navigator.push(
+        //           context, CupertinoPageRoute(builder: (_) => Chats()));
+        //     },
+        //   ),
+        //   SizedBox(width: 20.0),
+        // ],
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        children: [
+          StreamBuilderWrapper(
+            shrinkWrap: true,
+            stream: postRef.orderBy('timestamp', descending: true).snapshots(),
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (_, DocumentSnapshot snapshot) {
+              internetChecker();
+              PostModel posts = PostModel.fromJson(snapshot.data());
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 12.0),
+
+                // child: UserPost(post: posts),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  internetChecker() async {
+    bool result = await InternetConnectionChecker().hasConnection;
+    if (result == false) {
+      showInSnackBar('No Internet Connecton');
+    }
+  }
+
+  void showInSnackBar(String value) {
+    scaffoldKey.currentState.removeCurrentSnackBar();
+    scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(value)));
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,11 @@ import 'package:file_picker/file_picker.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
 import 'app/pages/announcement/announcement.dart';
 
 import 'app/pages/community/community_view.dart';
 import 'app/pages/home/home.dart';
 import 'app/pages/profile/profile.dart';
-
-
-
 
 Future<void> main() async {
   await Firebase.initializeApp();
@@ -39,10 +35,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   int _currentIndex = 0;
-  final List<Widget> _children = [Home(), Announcement(),Community(), Profile()];
+  final List<Widget> _children = [
+    Home(),
+    Announcement(),
+    Community(),
+    Profile()
+  ];
 
   void _onTap(int index) {
     setState(() {
@@ -50,11 +49,8 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-
-
   @override
   void initState() {
-
     super.initState();
   }
 
@@ -96,150 +92,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 icon: Icon(Icons.assignment_ind_outlined),
                 title: Text('프로필'),
               ),
-            ])
-
-
-        );
+            ]));
   }
-
-
 }
-
-
-
-
-
-//todo: aaa
-
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-//
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatefulWidget {
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
-//
-// class _MyAppState extends State<MyApp> {
-//   FirebaseFirestore firestore = FirebaseFirestore.instance;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     CollectionReference users = FirebaseFirestore.instance.collection('users');
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text("hello world"),
-//         ),
-//         body: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
-//           children: <Widget>[
-//             RaisedButton(
-//
-//               child:
-//                   Text("create button", style: TextStyle(color: Colors.white)),
-//               onPressed: () {
-//                 addData(users);
-//               },
-//               color: Colors.black,
-//               //클릭시 데이터를 추가해준다.
-//             ),
-//             FlatButton(
-//               color: Colors.blue,
-//               child: Text("read button", style: TextStyle(color: Colors.white)),
-//               onPressed: () {
-//                GetUserName();
-//                // var result =
-//                // Text(result.toString(), style: TextStyle(color: Colors.white));
-//                 //클릭시 데이터를 읽어준다
-//               },
-//             ),
-//             FlatButton(
-//               color: Colors.blue,
-//               child:
-//                   Text("update button", style: TextStyle(color: Colors.white)),
-//               onPressed: () {
-//                 //클릭시 데이터를 갱신해준다.
-//               },
-//             ),
-//             FlatButton(
-//               color: Colors.blue,
-//               child:
-//                   Text("delete button", style: TextStyle(color: Colors.white)),
-//               onPressed: () {
-//                 //클릭시 데이터를 삭제해 준다.
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Future<void> addData(CollectionReference users) {
-//     // Call the user's CollectionReference to add a new user
-//     return users
-//         .add({
-//           'full_name': "fullName", // John Doe
-//           'company': "company", // Stokes and Sons
-//           'age': "age" // 42
-//         })
-//         .then((value) => print("User Added"))
-//         .catchError((error) => print("Failed to add user: $error"));
-//   }
-//
-//   Future<void> readData(CollectionReference users) {
-//     // Call the user's CollectionReference to add a new user
-//     return users
-//         .add({
-//       'full_name': "fullName", // John Doe
-//       'company': "company", // Stokes and Sons
-//       'age': "age" // 42
-//     })
-//         .then((value) => print("User Added"))
-//         .catchError((error) => print("Failed to add user: $error"));
-//   }
-// }
-// class GetUserName extends StatelessWidget {
-//   final String documentId ;
-//
-//   GetUserName({this.documentId = "F1tLGBIHjk4SQrIoL1KM"});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     CollectionReference users = FirebaseFirestore.instance.collection('users');
-//
-//     return FutureBuilder<DocumentSnapshot>(
-//       future: users.doc(documentId).get(),
-//       builder:
-//           (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-//
-//         if (snapshot.hasError) {
-//           return Text("Something went wrong");
-//         }
-//
-//         if (snapshot.hasData && !snapshot.data.exists) {
-//           return Text("Document does not exist");
-//         }
-//
-//         if (snapshot.connectionState == ConnectionState.done) {
-//           Map<String, dynamic> data = snapshot.data.data();
-//           print("Full Name: ${data['full_name']} ${data['age']}");
-//           return Text("Full Name: ${data['full_name']} ${data['age']}");
-//
-//
-//         }
-//
-//         return Text("loading");
-//       },
-//     );
-//   }
-// }
