b3e89cc8f566952ecdaf11cb2a43dbaad550395c
JSH <wjdtkdgns234@naver.com>
2021-07-08 17:17:40 +0900

    에러 수정

--- lib/app/pages/bottom_tab/bottomTab_controller.dart
+++ lib/app/pages/bottom_tab/bottomTab_controller.dart
@@ -1,22 +1,18 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:synergy_flutter/app/pages/announcement/announcement.dart';
 import 'package:synergy_flutter/app/pages/community/community_view.dart';
 import 'package:synergy_flutter/app/pages/home/home_view.dart';
-import 'package:synergy_flutter/app/pages/login/login_view.dart';
-import 'package:synergy_flutter/app/pages/profile/profile.dart';
 import 'package:synergy_flutter/app/pages/profile/profile_view.dart';
-import 'package:synergy_flutter/app/pages/sign_up/signup_view.dart';
 import 'package:synergy_flutter/app/pages/welcome/welcome_presenter.dart';
 import 'package:synergy_flutter/app/pages/welcome/welcome_view.dart';
 
-import 'bottomTab_view.dart';
+
 
 class BottomTabController extends Controller {
   WelcomePresenter _welcomePresenter;
+
   BottomTabController() : _welcomePresenter = WelcomePresenter();
 
   int currentIndex = 0;
@@ -24,22 +20,24 @@ class BottomTabController extends Controller {
     HomePage(),
     Announcement(),
     Community(),
-    Profile()
+    ProfilePage()
   ];
+
   @override
   void initListeners() {}
 
   void onTap(int index) {
-      currentIndex = index;
-      refreshUI();
+    currentIndex = index;
+    refreshUI();
   }
 
   void signout() {
     FirebaseAuth.instance.signOut();
-    Navigator.pushAndRemoveUntil(getContext(),
-        MaterialPageRoute(builder: (context) => WelcomePage()), (route) => false);
+    Navigator.pushAndRemoveUntil(
+        getContext(),
+        MaterialPageRoute(builder: (context) => WelcomePage()),
+        (route) => false);
   }
-
 
   @override
   void onDisposed() {
